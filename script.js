cb26c1b74e871bf3e33d1aa77b3a69088081f5a6
Danny <danny.f.gomez@gmail.com>
2024-03-14 20:26:00 -0400

    Parse through spoonacular data to then send to nutritionix api

--- script.js
+++ script.js
@@ -23,6 +23,28 @@ document.getElementById('recipeForm').addEventListener('submit', function(e) {
         }
 
         var recipesHtml = '';
+        
+        // grab used ingredients from data and put them into a string
+        console.log(data)
+        
+        var recipeString = "";
+        var ingredientData = data[0]['usedIngredients'];
+        console.log(ingredientData);
+        
+        for (let i = 0; i < ingredientData.length; i++) {
+            var amount = ingredientData[i]['amount'];
+            var unitMeasurement = ingredientData[i]['unit'];
+            var ingredientName = ingredientData[i]['name'];
+
+            recipeString += `${amount} ${unitMeasurement} ${ingredientName}, `;
+        }
+       
+        console.log(recipeString)
+        getNutritionInfo(recipeString)
+        // -----------------------------------------------------------
+
+
+        
         data.forEach(recipe => {
             recipesHtml += `<div class="recipe-card"><h3>${recipe.title}</h3><img src="${recipe.image}" alt="Image of ${recipe.title}"></div>`;
         });
@@ -33,3 +55,30 @@ document.getElementById('recipeForm').addEventListener('submit', function(e) {
         document.getElementById('recipes').innerHTML = '<p>Please try again.</p>';
     });
 });
+
+function getNutritionInfo(ingredientsArg) {
+    var appID = "3f16e02e";
+    var appKey = "548ad8877714b47becab16723c1f292f";
+    var ingredients = ingredientsArg;
+
+    var url = "https://trackapi.nutritionix.com/v2/natural/nutrients";
+
+    fetch(url, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "x-app-id": appID,
+        "x-app-key": appKey,
+    },
+    body: JSON.stringify({
+        query: ingredients,
+    }),
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+
+}
