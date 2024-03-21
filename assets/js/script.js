34c316940c85a554e7e494e2f78d8a3c4c795e32
Spencer Klinkowize <spencerk@Spencers-MacBook-Pro-M3.local>
2024-03-21 18:42:29 -0400

    integreted all of Daniels additions and fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,39 +2,46 @@ document.getElementById("recipeForm").addEventListener("submit", function (e) {
   e.preventDefault();
   var ingredients = document.getElementById("ingredientsInput").value.trim();
   console.log("Ingredients submitted:", ingredients);
-  var allowedMissing = document.getElementById("missingIngredients").value || '0'; 
-  var apiKey = "6672e8c409a545128086b53cfbae1d59";
-  var apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&apiKey=${apiKey}&number=10&ignorePantry=true&ranking=1&maxMissedIngredients=${allowedMissing}`;
+  var allowedMissing =
+    document.getElementById("missingIngredients").value || "0";
+  var apiKey = "822983c348234fbba4210ca9dee9fecd";
+  // var apiKey = "6672e8c409a545128086b53cfbae1d59";
+  var apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&apiKey=${apiKey}&number=10&ignorePantry=true&ranking=1`;
 
   if (!ingredients) {
     console.log("No ingredients entered.");
     alert("Please enter at least one ingredient.");
     return;
   }
-
+  clearResults();
   document.getElementById("searchResults").classList.remove("hidden");
+
+  document.getElementById("resultsTitle").textContent = "Your Results";
+
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
       var recipesHtml = "";
+      var recipeResults = [];
       data.forEach((recipe) => {
-        recipesHtml += `<div class="recipe-card">
+        if (recipe.missedIngredientCount < allowedMissing) {
+          recipesHtml += `<div class="recipe-card">
               <h3 class="recipe-title" data-recipe='${JSON.stringify(
                 recipe
               )}' style="cursor:pointer;">${recipe.title}</h3>
               <img src="${recipe.image}" alt="Image of ${recipe.title}" />
-              <button onclick="toggleFavorite('${recipe.title.replace(
-                /'/g,
-                "\\'"
-              )}')">❤️</button>
+              <button onclick="toggleFavorite(${recipe})">❤️</button>
           </div>`;
-        // if (recipe.missedIngredientCount < 10) { 
+          recipeResults.push(recipe);
+        }
       });
       if (recipesHtml.length === 0) {
-        console.log(recipesHtml.length)
-        document.getElementById("recipes").innerHTML = '<p>No matching recipes. Please try again</p>'
+        console.log(recipesHtml.length);
+        document.getElementById("recipes").innerHTML =
+          "<p>No matching recipes. Please try again</p>";
       } else {
-      document.getElementById("recipes").innerHTML = recipesHtml;
+        sessionStorage.setItem("searchResults", JSON.stringify(recipeResults));
+        loadSessionStorage();
       }
     })
     .catch((error) => {
@@ -48,16 +55,13 @@ document.getElementById("hamburger").addEventListener("click", function () {
   document.getElementById("menu").classList.toggle("hidden");
 });
 
-function toggleFavorite(recipeTitle) {
-  console.log("Toggling favorite:", recipeTitle);
-  var favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  var index = favorites.indexOf(recipeTitle);
-  if (index > -1) {
-    favorites.splice(index, 1);
-  } else {
-    favorites.push(recipeTitle);
-  }
-  localStorage.setItem("favorites", JSON.stringify(favorites));
+function toggleFavorite(button) {
+  console.log("Toggling favorite:");
+  var recipe = JSON.parse(button.getAttribute('data-recipe'));
+
+  var favoritesArray = JSON.parse(localStorage.getItem("favorites")) || [];
+  favoritesArray.push(recipe)
+  localStorage.setItem("favorites", JSON.stringify(favoritesArray));
 }
 
 document.getElementById("showFavorites").addEventListener("click", function () {
@@ -66,34 +70,40 @@ document.getElementById("showFavorites").addEventListener("click", function () {
 });
 
 function showFavorites() {
+  clearSearch();
+  clearResults();
+  document.getElementById("searchResults").classList.remove("hidden");
+  var resultTitleEl = document.getElementById("resultsTitle");
+  resultTitleEl.textContent = "Your Favorites";
+  
   var favorites = JSON.parse(localStorage.getItem("favorites")) || [];
   console.log("Current favorites:", favorites);
-  var favoritesHtml = "<h2>Your Favorite Recipes</h2><ul>";
-  for (let i = 0; i < favorites.length; i++) {
-    var favorite = favorites[i];
-    favoritesHtml +=
-      "<li>" +
-      favorite +
-      " <button onclick=\"removeFavorite('" +
-      favorite.replace(/'/g, "\\'") +
-      "', event)\">❌</button></li>";
-  }
-  favoritesHtml += "</ul>";
+  var favoritesHtml = ""
+  if (favorites) {
+    favorites.forEach((recipe) => {
+      favoritesHtml += `<div class="recipe-card">
+      <h3 class="recipe-title" data-recipe='${JSON.stringify(
+        recipe
+      )}' style="cursor:pointer;">${recipe.title}</h3>
+      <img src="${recipe.image}" alt="Image of ${recipe.title}" />
+      <button onclick="removeFavorite(this)" data-recipe='${JSON.stringify(
+        recipe
+      )}'>❌</button></div>`;
+    });
   document.getElementById("recipes").innerHTML = favoritesHtml;
+  }
 }
 
-function removeFavorite(recipeTitle, event) {
-  event.stopPropagation();
-  console.log("Removing favorite:", recipeTitle);
-  var favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  var index = favorites.indexOf(recipeTitle);
-  if (index > -1) {
-    favorites.splice(index, 1);
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-    showFavorites();
+function removeFavorite(button) {
+  var recipe = JSON.parse(button.getAttribute('data-recipe'));
+  var removeRecipeID = recipe.id;
+  
+  var favoritesArray = JSON.parse(localStorage.getItem("favorites"));
+  var newFavoritesArray = favoritesArray.filter(item => item.id !== removeRecipeID);
+  localStorage.setItem("favorites", JSON.stringify(newFavoritesArray));
+  showFavorites();
   }
-}
-// ----------------------------------------------------------------
+
 
 function prepareForDetailsPage(recipeData) {
   console.log("Preparing for details page:", recipeData);
@@ -107,3 +117,59 @@ document.addEventListener("click", function (event) {
     prepareForDetailsPage(recipeData);
   }
 });
+
+function loadSessionStorage() {
+  var sessionData = JSON.parse(sessionStorage.getItem("searchResults"));
+  var recipesHtml = "";
+  if (sessionData) {
+    sessionData.forEach((recipe) => {
+      recipesHtml += `<div class="recipe-card">
+      <h3 class="recipe-title" data-recipe='${JSON.stringify(
+        recipe
+      )}' style="cursor:pointer;">${recipe.title}</h3>
+      <img src="${recipe.image}" alt="Image of ${recipe.title}" />
+      <button onclick="toggleFavorite(this)" data-recipe='${JSON.stringify(
+        recipe
+      )}'>❤️</button></div>`;
+    });
+    document.getElementById("recipes").innerHTML = recipesHtml;
+    document.getElementById("searchResults").classList.remove("hidden");
+    console.log("results pulled from session storage");
+    createClearButton();
+  }
+}
+
+function clearSearch() {
+  var ingredientsInputEl = document.getElementById("ingredientsInput");
+  ingredientsInputEl.value = "";
+  var mealTypeInputEl = document.getElementById("mealType");
+  mealTypeInputEl.value = "";
+  var missingIngredientsEl = document.getElementById("missingIngredients");
+  missingIngredientsEl.value = "";
+}
+
+function createClearButton() {
+  var searchResultsEl = document.getElementById("searchResults");
+  var clearButton = document.createElement("button");
+  clearButton.setAttribute("id", "clearButton");
+  clearButton.classList.add("bg-blue-500", "text-white", "p-2", "rounded");
+  clearButton.textContent = "Clear Search";
+  searchResultsEl.append(clearButton);
+
+  clearButton.addEventListener("click", function () {
+    clearResults();
+    clearSearch();
+  });
+}
+
+function clearResults() {
+  var searchResultsEl = document.getElementById("searchResults");
+  var clearButtonEl = document.getElementById("clearButton");
+  sessionStorage.removeItem("searchResults");
+  searchResultsEl.classList.add("hidden");
+  if (clearButtonEl) {
+    searchResultsEl.removeChild(clearButtonEl);
+  }
+}
+
+loadSessionStorage();
