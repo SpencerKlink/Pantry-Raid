348e09789554fac5576f7f0e5d2fec63ad1072d4
Nicole Greenspun <greenspun.nicole@gmail.com>
2024-04-02 16:48:05 -0400

    Update details.js
    Merge error in VSC

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,17 +1,12 @@
 var recipeData = JSON.parse(localStorage.getItem("currentRecipeData"));
-
 stringifyRecipeIngredients(recipeData);
-
 function displayRecipeDetails(recipeData) {
   var detailsElement = document.getElementById("recipeDetails");
   var titleEl = document.getElementById("recipeTitle");
-  var html = `
-        <img src="${recipeData.image}" alt="Image of ${recipeData.title}" />
-    `;
+  var html = `<img src="${recipeData.image}" onerror="this.onerror=null;this.src='./assets/images/missing-image2.png';" alt="Image of ${recipeData.title}" class="mx-auto block max-w-4xl w-full h-auto" />`;
   detailsElement.innerHTML = html;
   titleEl.innerHTML = recipeData.title;
 }
-
 function getRecipeUrl(recipeData) {
   var id = recipeData["id"];
   var title = recipeData["title"].replaceAll(" ", "-");
@@ -20,15 +15,12 @@ function getRecipeUrl(recipeData) {
   console.log(recipeUrl);
   getRecipeInstructions(recipeUrl);
 }
-
 function stringifyRecipeIngredients(recipeData) {
   console.log(recipeData);
-
   var recipeString = "";
   var usedIngredientData = recipeData["usedIngredients"];
-  var missedIngredientData = recipeData["missedIngredients"]
+  var missedIngredientData = recipeData["missedIngredients"];
   console.log(usedIngredientData);
-
   for (let i = 0; i < usedIngredientData.length; i++) {
     var amount = usedIngredientData[i]["amount"];
     var unitMeasurement = usedIngredientData[i]["unitLong"];
@@ -36,7 +28,6 @@ function stringifyRecipeIngredients(recipeData) {
     recipeString += `${amount} ${unitMeasurement} ${ingredientName}, `;
     console.log(ingredientName);
   }
-
   // added loop to add missing ingredients to ingredient list and nutrition facts
   for (let i = 0; i < missedIngredientData.length; i++) {
     var amount = missedIngredientData[i]["amount"];
@@ -45,13 +36,11 @@ function stringifyRecipeIngredients(recipeData) {
     recipeString += `${amount} ${unitMeasurement} ${ingredientName}, `;
     console.log(ingredientName);
   }
-
   getRecipeUrl(recipeData);
   displayRecipeDetails(recipeData);
   displayIngredients(recipeString);
-//   getNutritionInfo(recipeString);
+  getNutritionInfo(recipeString);
 }
-
 function displayIngredients(recipeString) {
   var recipeIngredientsEl = document.getElementById("recipeIngredients");
   var ingredientsArray = recipeString.split(",");
@@ -61,55 +50,69 @@ function displayIngredients(recipeString) {
     recipeIngredientsEl.append(ingredientListItem);
   }
 }
-
 function getRecipeInstructions(recipeUrlArg) {
   fetch(recipeUrlArg)
     .then((response) => response.text())
     .then((html) => {
       var parser = new DOMParser();
       var doc = parser.parseFromString(html, "text/html");
-      var paragraph = doc.querySelector(".recipeInstructions") ||  doc.querySelector("#detailedInstructionsMention");
-      var paragraphText = paragraph.innerHTML; // Get the text content of the paragraph
+      var paragraph =
+        doc.querySelector(".recipeInstructions") ||
+        doc.querySelector("#detailedInstructionsMention");
+      var paragraphText = paragraph.innerHTML;
       console.log(paragraphText);
       var recipeInstructionsEl = document.getElementById("recipeInstructions");
       recipeInstructionsEl.innerHTML =
         paragraphText +
-        ` <a href="${recipeUrlArg}">For more visit Spoonacular.com</a>`;
+        ` <a class="font-bold" href="${recipeUrlArg}">For more visit Spoonacular.com</a>`;
     })
     .catch((error) => console.error("Error fetching the webpage:", error));
 }
-
 document.addEventListener("DOMContentLoaded", function () {
   var recipeId = localStorage.getItem("currentRecipeId");
   if (recipeId) {
     fetchNutritionalInfo(recipeId);
   }
 });
-
-function fetchNutritionalInfo(recipeId) {
-  var ingredients = "Example ingredients list";
-  getNutritionInfo(ingredients);
-}
-
-function displayNutritionData(data) {
-  var detailsElement = document.getElementById("nutritionDetails");
-  console.log(data);
-  console.log(data.foods);
+function displayNutritionData() {
+  var data = JSON.parse(localStorage.getItem("currentRecipeNutrition"));
+  var modalInfoEl = document.getElementById("nutritionInfo");
   if (data.foods && data.foods.length > 0) {
-    var nutritionInfo = data.foods
-      .map((food) => `<p>${food.food_name}: ${food.nf_calories} calories</p>`)
-      .join("");
-    detailsElement.innerHTML = nutritionInfo;
+    nutritionData = data["foods"];
+    console.log(nutritionData);
+    var cals = 0;
+    var fats = 0;
+    var carbs = 0;
+    var proteins = 0;
+    var sugars = 0;
+    var sodium = 0;
+    for (let i = 0; i < nutritionData.length; i++) {
+      cals += nutritionData[i]["nf_calories"];
+      fats += nutritionData[i]["nf_total_fat"];
+      carbs += nutritionData[i]["nf_total_carbohydrate"];
+      proteins += nutritionData[i]["nf_protein"];
+      sugars += nutritionData[i]["nf_sugars"];
+      sodium += nutritionData[i]["nf_sodium"];
+    }
+    var totalMealNutrition = `<ul>
+      <li>Cals:${Math.ceil(cals)}</li>
+      <li>Fats:${Math.ceil(fats)}g</li>
+      <li>Carbs: ${Math.ceil(carbs)}g</li>
+      <li>Proteins: ${Math.ceil(proteins)}g</li>
+      <li>Sugars: ${Math.ceil(sugars)}g</li>
+      <li>Sodium: ${Math.ceil(sodium)}mg</li>
+      </ul>`;
+    modalInfoEl.innerHTML = totalMealNutrition;
   } else {
-    detailsElement.innerHTML = "<p>No nutritional data found.</p>";
+    modalInfoEl.innerHTML = "<p>No nutritional data found.</p>";
   }
 }
-
 function getNutritionInfo(ingredientsArg) {
+  //   var appID = "3f16e02e";
+  //   var appKey = "548ad8877714b47becab16723c1f292f";
   var appID = "c801545b";
   var appKey = "a70cf301943e3e0868d65610e47736c3";
   var url = "https://trackapi.nutritionix.com/v2/natural/nutrients";
-
   fetch(url, {
     method: "POST",
     headers: {
@@ -122,7 +125,23 @@ function getNutritionInfo(ingredientsArg) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      displayNutritionData(data);
+      localStorage.setItem("currentRecipeNutrition", JSON.stringify(data));
     })
     .catch((error) => console.error("Error fetching nutritional data:", error));
 }
+function openModal() {
+  document.getElementById("nutritionModal").classList.remove("hidden");
+  displayNutritionData();
+}
+function closeModal(event) {
+  if (event) {
+    event.stopPropagation();
+  }
+  document.getElementById("nutritionModal").classList.add("hidden");
+}
+document.addEventListener("DOMContentLoaded", () => {
+  var closeModalButton = document.getElementById("closeModalButton");
+  if (closeModalButton) {
+    closeModalButton.addEventListener("click", closeModal);
+  }
+});
